05bbe916b128702ad71555bbc42074be8063a006
bensamir <mohamedamine.bensamir@esprit.tn>
2022-03-08 23:52:34 +0100

    first commit

--- src/main/java/tn/esprit/spring/repository/IDepratmentRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.spring.entity.Departement;
-
-public interface IDepratmentRepository extends CrudRepository<Departement, Long> {
-}
--- src/main/java/tn/esprit/spring/repository/IMembresOfCompany.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.spring.entity.MembersOfCompany;
-
-public interface IMembresOfCompany extends CrudRepository<MembersOfCompany,Long> {
-
-    boolean existsByNid(String nid);
-}
